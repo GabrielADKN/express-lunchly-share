bee9d36e293f80d917dcb09823b8fe75185cca5b
gabrieladkn <adoukonoubienheureux@gmail.com>
2024-02-28 12:44:39 -0500

    adding fullname function and add

--- db.js
+++ db.js
@@ -1,8 +1,19 @@
 /** Database for lunchly */
 
-const pg = require("pg");
+const { Client } = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+    DB_URI = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/lunchly_test`;
+} else {
+    DB_URI = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/lunchly`;
+}
+
+console.log(DB_URI);
+let db = new Client({
+    connectionString: DB_URI
+});
 
 db.connect();
 
